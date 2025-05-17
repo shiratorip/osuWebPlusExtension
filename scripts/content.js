c344ada451e9e86bc1fc40a82fda8ec7ced6b258
shira <kirillbabko.bda@gmail.com>
2025-05-17 16:25:59 +0200

    added player - not working

--- scripts/content.js
+++ scripts/content.js
@@ -107,12 +107,59 @@ function checkForElements() {
         const link2 = "https://assets.ppy.sh/beatmaps/"+bId+"/covers/list@2x.jpg";
         beatmapset_cover_background.style = `--bg: url("${link}"); --bg-2x: url("${link2}");`;
 
+        const beatmapset_panel_content = document.createElement('div');
+        beatmapset_panel_content.className = 'beatmapset-panel__content';
+
+        const beatmapset_panel_play_container = document.createElement('div');
+        beatmapset_panel_play_container.className = 'beatmapset-panel__play-container';
+        
+        const beatmapset_panel_play = document.createElement('button');
+        beatmapset_panel_play.className = 'beatmapset-panel__play js-audio--play';
+        beatmapset_panel_play.type = 'button';
+
+        const play_button = document.createElement('span');
+        play_button.className = 'play-button';
+
+        const beatmapset_panel_play_progress = document.createElement('div');
+        beatmapset_panel_play_progress.className = 'beatmapset-panel__play-progress';
+
+        const beatmapset_panel_play_icons = document.createElement('div');
+        beatmapset_panel_play_icons.className = 'beatmapset-panel__play-icons';
+
+        const circular_progress = document.createElement('div');
+        circular_progress.className = 'circular-progress circular-progress--beatmapset-panel';
+        circular_progress.title = '0 / 1';
+
+        const circular_progress_label = document.createElement('div');
+        circular_progress_label.className = 'circular-progress__label';
+        circular_progress_label.textContent = '1';
+
+        const circular_progress_slice = document.createElement('div');
+        circular_progress_slice.className = 'circular-progress__slice';
+
+        const circular_procress_circle = document.createElement('div');
+        circular_procress_circle.className = 'circular-progress__circle';
+
+        const circular_progress_circle_fill = document.createElement('div');
+        circular_progress_circle_fill.className = 'circular-progress__circle circular-progress__circle--fill';
+
+        circular_progress_slice.appendChild(circular_procress_circle);
+        circular_progress_slice.appendChild(circular_progress_circle_fill);
+        circular_progress.appendChild(circular_progress_label);
+        circular_progress.appendChild(circular_progress_slice);
+        beatmapset_panel_play_progress.appendChild(circular_progress);
+        beatmapset_panel_play.appendChild(play_button);
+        beatmapset_panel_play_container.appendChild(beatmapset_panel_play);
+        beatmapset_panel_play_container.appendChild(beatmapset_panel_play_progress);
+        beatmapset_panel_play_container.appendChild(beatmapset_panel_play_icons);
+        beatmapset_panel_content.appendChild(beatmapset_panel_play_container);
 
 
 
         beatmapset_panel_cover_col_play.appendChild(beatmapset_cover_background);
         beatmapset_panel_cover_container.appendChild(beatmapset_panel_cover_col_play);
         playercontainer.appendChild(beatmapset_panel_cover_container);
+        playercontainer.appendChild(beatmapset_panel_content);
         playercontainer.appendChild(score_data_wrapper);
         group.prepend(playercontainer);
 
